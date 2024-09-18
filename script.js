f2878bf88095bb3a320adc6028a97e2cf4f66ac9
Aditya Singh <adityasingh740745@gmail.com>
2024-09-18 09:52:37 +0530

    add gooey, textSwipe effect

--- script.js
+++ script.js
@@ -164,25 +164,35 @@ function magnetEffect() {
     });
 }
 
-function textSwipeEffect() {
-    let frames = document.querySelectorAll(".frame")
+function textSwipeEffect(textContainer, swipeOffsetY, swipeDuration, hoverElementSelector = null) {
+    let frames = document.querySelectorAll(textContainer)
 
-    frames.forEach(function (frame) {
-        frame.addEventListener("mousemove", function () {
+    frames.forEach(function (frame, index) {
+
+        let hoverElement = hoverElementSelector ? document.querySelectorAll(hoverElementSelector)[index] : frame
+
+        hoverElement.addEventListener("mousemove", function () {
             gsap.to(frame.children, {
-                y: "-1.6vw",
-                duration: .55,
+                y: swipeOffsetY,
+                duration: swipeDuration,
             })
         })
 
-        frame.addEventListener("mouseleave", function () {
+        hoverElement.addEventListener("mouseleave", function () {
             gsap.to(frame.children, {
                 y: 0,
-                duration: .55,
+                duration: swipeDuration,
             })
         })
-
     })
+}
+
+function gooeyEffect() {
+    Shery.imageEffect(".project-image", {
+        style: 6,
+        gooey: true,
+        config: { "noiseDetail": { "value": 9.16, "range": [0, 100] }, "distortionAmount": { "value": 3.74, "range": [0, 10] }, "scale": { "value": 59.54, "range": [0, 100] }, "speed": { "value": 0.51, "range": [0, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749691738595 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.53, "range": [0, 10] }, "metaball": { "value": 0.47, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.34, "range": [0, 2] }, "noise_scale": { "value": 11.45, "range": [0, 100] } }
+    });
 }
 
 locoScroll()
@@ -190,4 +200,6 @@ cursor()
 progressCounter()
 preloaderAnimations()
 magnetEffect()
-textSwipeEffect()
+textSwipeEffect(".frame", "-1.6vw", 0.55)
+textSwipeEffect(".header-frame", "-2.1vw", 0.45, ".project-item")
+gooeyEffect()
